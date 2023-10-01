ab17ec0fcbf672d8adeaf2589a0cabefc0012aef
june <66232436+ryu9663@users.noreply.github.com>
2023-10-01 23:25:59 +0900

    feat: BLOG-7 notion 데이터를  md로 변환 (#8)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,14 +10,13 @@ export const metadata: Metadata = {
 
 export default async function About() {
   const posts = await getPosts();
-  console.log("포스트aboutPage.tsx", posts);
 
   return (
     <main>
       hello world About
       <div style={{ marginTop: "200px" }}>
         {posts.map((post, i) => (
-          <div key={i}>{post}</div>
+          <Markdown key={i}>{post}</Markdown>
         ))}
       </div>
     </main>
