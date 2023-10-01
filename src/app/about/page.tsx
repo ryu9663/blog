a148a03d95a3c21590a7a8dc4509cf8bc3721cf8
june <ryu9663@naver.com>
2023-10-01 13:34:59 +0900

    feat: BLOG-4 전역변수

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import "@/styles/_global.scss";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Next About",
+  description: "gjgj",
+};
+
+export default function About() {
+  return <main>hello world About</main>;
+}
