4eee445d6e8837e1237809e4a3cc4f304c714029
june <66232436+ryu9663@users.noreply.github.com>
2023-10-02 00:07:51 +0900

    feat: BLOG-8 ISR 설정 (5분) (#9)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 import { getPosts } from "@/app/api/notion";
 import "@/styles/_global.scss";
+import { REVALIDATE_TIME } from "@/utils/revalidate";
 import { Metadata } from "next";
 import Markdown from "react-markdown";
 
@@ -7,6 +8,8 @@ export const metadata: Metadata = {
   title: "Create Next About",
   description: "gjgj",
 };
+
+export const revalidate = REVALIDATE_TIME;
 
 export default async function About() {
   const posts = await getPosts();
