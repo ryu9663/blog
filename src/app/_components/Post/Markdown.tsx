40dd8807afd5e86b112e9faa25d42145d4853bcd
june <ryu9663@naver.com>
2023-11-21 13:56:49 +0900

    favicon

--- src/app/_components/Post/Markdown.tsx
+++ src/app/_components/Post/Markdown.tsx
@@ -4,7 +4,6 @@ import MarkdownLibrary from "react-markdown";
 import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import remarkGfm from "remark-gfm";
 import Image from "next/image";
-import { ResponsiveImageType } from "@/types";
 import { NO_IMAGE } from "@/utils/constant";
 
 interface MarkdownProps {
