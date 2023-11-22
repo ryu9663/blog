c2af9c369960a47ad830d46a0e1a1b5b561deff9
june <66232436+ryu9663@users.noreply.github.com>
2023-11-22 23:54:20 +0900

    feat: BLOG-39 이미지 확대 기능, 마우스 호버시 사전로딩 (#35)
    * feat: BLOG-39 이미지 확대 기능

--- src/app/post/[id]/Markdown.tsx
+++ src/app/post/[id]/Markdown.tsx
@@ -4,8 +4,7 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import MarkdownLibrary from "react-markdown";
 import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import remarkGfm from "remark-gfm";
-import Image from "next/image";
-import { NO_IMAGE } from "@/utils/constant";
+import { MarkdownImage } from "@/app/_components/MarkdownImage";
 
 interface MarkdownProps {
   children: string;
@@ -30,20 +29,7 @@ export const Markdown = ({ children }: MarkdownProps) => {
           },
           img: (data) => {
             const { src, alt } = data;
-            const urlParams = new URLSearchParams(
-              new URL(src || NO_IMAGE.src).search
-            );
-            const width = Number(urlParams.get("w"));
-            const height = Number(urlParams.get("h"));
-
-            return (
-              <Image
-                width={width || 600}
-                height={height || 300}
-                src={src || NO_IMAGE.src}
-                alt={alt || NO_IMAGE.alt}
-              />
-            );
+            return <MarkdownImage src={src} alt={alt} />;
           },
         }}
       >
