e8a5bb4356339a5b10aa68f89e1094e9b6090879
june <ryu9663@naver.com>
2023-11-23 00:38:08 +0900

    feat: BLOG-40 실험: next/image를 img로 바꾸면 preload한 img로드가 빠를까

---
+++ src/app/_components/MarkdownImage/BigImageModal/index.tsx
@@ -0,0 +1,37 @@
+import { ImageSrcAltType } from "@/app/_components/MarkdownImage";
+import Image from "next/image";
+import styles from "./index.module.scss";
+
+type RequiredImageSrcAltType = {
+  [K in keyof ImageSrcAltType]-?: ImageSrcAltType[K];
+};
+interface BigImageModalProps extends RequiredImageSrcAltType {
+  isOpen: boolean;
+  handleClose: () => void;
+}
+export const BigImageModal = ({
+  isOpen,
+  handleClose,
+  src,
+  alt,
+}: BigImageModalProps) => {
+  return (
+    <>
+      {isOpen && (
+        <div
+          className={`${styles.backdrop} ${styles["priority-0"]}`}
+          onClick={handleClose}
+        >
+          <div className={`${styles.fill_img_wrapper} ${styles["priority-0"]}`}>
+            <img
+              onClick={(e) => e.stopPropagation()}
+              className={styles.img}
+              src={src}
+              alt={alt}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
