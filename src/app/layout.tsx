7223aca0bdcf71f69f7f035d3fcfef4bc5a9fae9
june <ryu9663@naver.com>
2023-11-22 12:26:02 +0900

    fix: favicon을 layout으로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon-32x32.png" />
+      </head>
       <body className={inter.className}>
         <SidebarWrapper />
         <main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,6 @@ import { Posts } from "@/app/_components/Posts";
 export const metadata: Metadata = {
   title: "류준열 기술 블로그",
   description: "프론트엔드 개발자 류준열 이력서",
-  icons: {
-    icon: "/favicon-32x32.png",
-  },
 };
 
 export default async function Home() {
