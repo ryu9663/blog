f664114d8da0cc7f40e18496e4d689ba319cf907
june <ryu9663@naver.com>
2024-01-02 14:21:01 +0900

    k

--- src/app/_components/Provider/index.tsx
+++ src/app/_components/Provider/index.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { motion, useScroll } from "framer-motion";
 import React, { PropsWithChildren } from "react";
 import styles from "./index.module.scss";
 import { useSidebarStore } from "@/app/_components/Sidebar/index.store";
 
 export const Provider = ({ children }: PropsWithChildren) => {
+  const { scrollYProgress } = useScroll();
+  console.log(scrollYProgress);
   const isSidebarOn = useSidebarStore((state) => state.isSidebarOn);
   return (
     <main
@@ -11,6 +14,10 @@ export const Provider = ({ children }: PropsWithChildren) => {
         isSidebarOn ? styles.sidebar_opened : ""
       }`}
     >
+      <motion.div
+        className={styles["progress-bar"]}
+        style={{ scaleX: scrollYProgress }}
+      />
       {children}
     </main>
   );
