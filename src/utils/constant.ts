3fc03b29cba042194949310e9d03354915c508e2
june <66232436+ryu9663@users.noreply.github.com>
2024-01-14 00:15:43 +0900

    fix: next 공식문서가 시키는대로 환경변수 이름 변경 (#67)

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,3 @@
-export const DATO_API_TOKEN = process.env.NEXT_DATOCMS_API_TOKEN;
 export const REVALIDATE_TIME = 10; // 300 seconds
 export const DATO_API_END_POINT = "https://graphql.datocms.com/";
 export const IMAGE_SIZE_IN_POSTS = {
