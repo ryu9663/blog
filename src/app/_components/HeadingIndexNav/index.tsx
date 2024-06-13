dcc887febc8077865374c270722b24ebd2ce67dc
june <ryu9663@naver.com>
2024-06-13 23:02:28 +0900

    test/1

--- src/app/_components/HeadingIndexNav/index.tsx
+++ src/app/_components/HeadingIndexNav/index.tsx
@@ -7,6 +7,7 @@ interface HeadingIndexProps {
 }
 export const HeadingIndexNav = ({ markdown }: HeadingIndexProps) => {
   const headingIndexes = extractHeadings(markdown);
+  console.log("hi");
   return (
     <nav className={styles.heading_index_nav}>
       {headingIndexes.map((heading, i) => (
