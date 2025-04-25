e0104d91317b26afbded88680bc47a75efd4ca46
june <ryu9663@naver.com>
2025-04-25 23:24:57 +0900

    fix: build error

--- src/app/_components/SidebarWrapper/index.tsx
+++ src/app/_components/SidebarWrapper/index.tsx
@@ -3,12 +3,8 @@ import { PostType } from "@/types";
 import { getCategories } from "@/app/api/dato/getCategories";
 
 import { formatSidebarData } from "@/app/_components/SidebarWrapper/utils";
-import dynamic from "next/dynamic";
 import { getPosts } from "@/app/api/dato/getPosts";
-
-const Sidebar = dynamic(() => import("@/app/_components/Sidebar"), {
-  ssr: false,
-});
+import Sidebar from "@/app/_components/Sidebar";
 
 export const SidebarWrapper = async () => {
   const { allArticles: articles } = await getPosts<{
